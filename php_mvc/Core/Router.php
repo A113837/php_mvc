770d8f5d0b9ee2d7bf7f0c8223cc800ef6f0dd39
A113837 <padi196@live.com>
2020-02-19 14:06:53 +0800

    Update Router.php

--- php_mvc/Core/Router.php
+++ php_mvc/Core/Router.php
@@ -6,6 +6,8 @@ class Router
 
 	protected $routes = [];
 
+	protected $params = [];
+
 
 	public function add($route, $params)
 	{
@@ -15,6 +17,60 @@ class Router
 	public function getRoutes()
 	{
 		return $this->routes;
+	}
+
+	/**
+     * Match the route to the routes in the routing table, setting the $params
+     * property if a route is found.
+     *
+     * @param string $url The route URL
+     *
+     * @return boolean  true if a match found, false otherwise
+     */
+
+	public function match($url)
+	{
+		/*foreach ($this->routes as $route => $params) {
+			if($url == $route)
+			{
+				$this->params = $params;
+				return true;
+			}
+		}
+		return false;*/
+
+		$reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+		if(preg_match($reg_exp, $url, $matches))
+		{
+			//Get named capture group values
+			$params = [];
+
+			foreach($matches as $key => $match)
+			{
+				if(is_string($key))
+				{
+					$params[$key] = $match;
+				}
+			}
+
+			$this -> params = $params;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	/**
+     * Get the currently matched parameters
+     *
+     * @return array
+     */
+	public function getParams()
+	{
+		return $this->params;
 	}
 }
 
