ce0a8947c4f9e2cd5060dd6d713c12eb048c28ac
A113837 <padi196@live.com>
2020-02-20 15:12:18 +0800

    Add files via upload

---
+++ 024/php_mvc/App/Controllers/Posts.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+class Posts
+{
+	public function index()
+	{
+		echo 'Hello form the index aciton in the Posts controller';
+	}
+
+	public function addNew()
+	{
+		echo 'Hello from the addNew action in the Posts controller';
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+ ?>
